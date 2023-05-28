32e2090152f63231919d80c32cbf075e3d8e0240
Bjarne Jensen <b.bjarnejensen@web.de>
2023-05-28 09:36:50 +0200

    Update spring initializr

---
+++ src/main/java/bj_WebRestService_MongoDB_com/example/WebRestService_MongoDB/WebRestServiceMongoDbApplication.java
@@ -0,0 +1,13 @@
+package bj_WebRestService_MongoDB_com.example.WebRestService_MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRestServiceMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRestServiceMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bj_WebRestService_MongoDB_com/example/WebRestService_MongoDB/WebRestServiceMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package bj_WebRestService_MongoDB_com.example.WebRestService_MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRestServiceMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
